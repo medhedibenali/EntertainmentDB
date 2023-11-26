9a28737b18a3517ed4d8ca1901b5cd297aa1ac6e
Mohamed Hedi Ben Ali <mohamedhedi.benali02@gmail.com>
2023-11-26 19:26:02 +0100

    feat: add Game model

---
+++ Models/Game.cs
@@ -0,0 +1,20 @@
+namespace EntertainmentDB.Models;
+
+public class Game : Media
+{
+    public string Synopsis { get; set; } = "";
+
+    public virtual ICollection<Company>? Developers { get; set; }
+
+    public virtual ICollection<Company>? Publishers { get; set; }
+
+    public virtual ICollection<Person>? Directors { get; set; }
+
+    public virtual Franchise? Franchise { get; set; }
+
+    public virtual ICollection<Track>? Soundtrack { get; set; }
+
+    public virtual ICollection<Platform>? Platforms { get; set; }
+
+    public virtual ICollection<Mode>? Modes { get; set; }
+}
