6ee53dc00ef82955a40f05c88547fab3a9feddcd
Mohamed Hedi Ben Ali <mohamedhedi.benali02@gmail.com>
2023-11-26 19:15:03 +0100

    refactor: rename Name to Title in Media

--- Models/Media.cs
+++ Models/Media.cs
@@ -4,7 +4,7 @@ public abstract class Media
 {
     public Guid Id { get; set; }
 
-    public string Name { get; set; } = "";
+    public string Title { get; set; } = "";
 
     public DateOnly ReleaseDate { get; set; }
 
