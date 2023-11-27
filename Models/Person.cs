73cce94f865ddf2b3e220f3ed87b63e1169d76d7
Ayadi-Nassim <nassim.ayadi@insat.ucar.tn>
2023-11-27 21:52:44 +0100

    Modifying String? to String val = ";

--- Models/Person.cs
+++ Models/Person.cs
@@ -2,10 +2,10 @@ namespace EntertainmentDB.Models;
 
 public class Person
 {
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
     public DateOnly Birthday { get; set; }
-    public string? Origin { get; set; }
+    public string Origin { get; set; } = "";
     public virtual ICollection<Media>? Products { get; set; }
     public virtual ICollection<Franchise>? Franchises { get; set; }
 
