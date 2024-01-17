0ebcd740d78a38a1a9854124d69b7efb63d48116
Anas Boukchina <mohamedanas.boukchina@carnet.tn>
2024-01-17 11:28:40 +0100

    added genre tag and production modesl

---
+++ Models/Production.cs
@@ -0,0 +1,9 @@
+﻿namespace EntertainmentDB.Models
+{
+    public class Production
+    {
+        public string Name { get; set; } = "";
+        public string Origin { get; set; } = "";
+        public virtual ICollection<Media>? Media { get; set; }
+    }
+}
