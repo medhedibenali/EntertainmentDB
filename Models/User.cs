07955e6fa4adbb139bb2cf3bcc047c10601a10e9
Mohamed Hedi Ben Ali <mohamedhedi.benali02@gmail.com>
2024-01-19 00:58:30 +0100

    fix: make the Login and Password required

--- Models/User.cs
+++ Models/User.cs
@@ -4,9 +4,9 @@ public class User
 {
     public Guid Id { get; set; }
 
-    public string? Login { get; set; }
+    public string Login { get; set; } = "";
 
-    public string? Password { get; set; }
+    public string Password { get; set; } = "";
 
     public virtual ICollection<Media>? Favourites { get; set; }
 }
