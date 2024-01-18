6658b87dc32b8c3585ce338bc6b64603075133e5
Mohamed Hedi Ben Ali <mohamedhedi.benali02@gmail.com>
2024-01-19 00:17:09 +0100

    feat: add the DbSets to the DbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,39 @@
 using Microsoft.EntityFrameworkCore;
+using EntertainmentDB.Models;
 
 namespace EntertainmentDB.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
+    public DbSet<Book> Books { get; set; }
 
+    public DbSet<Company> Companies { get; set; }
+
+    public DbSet<Episode> Episodes { get; set; }
+
+    public DbSet<Franchise> Franchises { get; set; }
+
+    public DbSet<Game> Games { get; set; }
+
+    public DbSet<Genre> Genres { get; set; }
+
+    public DbSet<Mode> Modes { get; set; }
+
+    public DbSet<Movie> Movies { get; set; }
+
+    public DbSet<Person> Persons { get; set; }
+
+    public DbSet<Platform> Platforms { get; set; }
+
+    public DbSet<Production> Productions { get; set; }
+
+    public DbSet<Season> Seasons { get; set; }
+
+    public DbSet<Show> Shows { get; set; }
+
+    public DbSet<Tag> Tags { get; set; }
+
+    public DbSet<Track> Tracks { get; set; }
+
+    public DbSet<User> Users { get; set; }
 }
