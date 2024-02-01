af1ab42ff2c3d3a16765ab422790a192453253d2
Mohamed Hedi Ben Ali <mohamedhedi.benali02@gmail.com>
2024-02-01 15:42:35 +0100

    feat: add CrudController

---
+++ Controllers/CrudController.cs
@@ -0,0 +1,85 @@
+using EntertainmentDB.Models;
+using EntertainmentDB.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntertainmentDB.Controllers;
+
+public abstract class CrudController<TEntity>(ICrudService<TEntity> entityService) : ControllerBase where TEntity : class, IEntity<Guid>
+{
+    protected readonly ICrudService<TEntity> entityService = entityService;
+
+    [HttpGet]
+    public virtual ActionResult<IEnumerable<TEntity>> Get()
+    {
+        return Ok(entityService.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public virtual ActionResult<TEntity> Get(Guid id)
+    {
+        var entity = entityService.GetById(id);
+
+        if (entity == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(entity);
+    }
+
+    [HttpPut("{id}")]
+    public virtual IActionResult Put(Guid id, TEntity entity)
+    {
+        if (id != entity.Id)
+        {
+            return BadRequest();
+        }
+
+        try
+        {
+            entityService.Update(entity);
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!TEntityExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpPost]
+    public virtual ActionResult<TEntity> Post(TEntity entity)
+    {
+        entityService.Insert(entity);
+
+        return CreatedAtAction("Get", new { id = entity.Id }, entity);
+    }
+
+    [HttpDelete("{id}")]
+    public virtual IActionResult Delete(Guid id)
+    {
+        var entity = entityService.GetById(id);
+
+        if (entity == null)
+        {
+            return NotFound();
+        }
+
+        entityService.Delete(entity);
+
+        return NoContent();
+    }
+
+    protected virtual bool TEntityExists(Guid id)
+    {
+        return entityService.GetById(id) != null;
+    }
+}
