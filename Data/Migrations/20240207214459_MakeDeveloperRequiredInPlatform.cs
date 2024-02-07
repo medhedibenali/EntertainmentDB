a03b0961cfad2e93d815a140bb6bdaed9a9ed714
Mohamed Hedi Ben Ali <mohamedhedi.benali02@gmail.com>
2024-02-07 23:51:27 +0100

    feat: add PlatformController

---
+++ Data/Migrations/20240207214459_MakeDeveloperRequiredInPlatform.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntertainmentDB.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeDeveloperRequiredInPlatform : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Platforms_Companies_DeveloperId",
+                table: "Platforms");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DeveloperId",
+                table: "Platforms",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Platforms_Companies_DeveloperId",
+                table: "Platforms",
+                column: "DeveloperId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Platforms_Companies_DeveloperId",
+                table: "Platforms");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DeveloperId",
+                table: "Platforms",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Platforms_Companies_DeveloperId",
+                table: "Platforms",
+                column: "DeveloperId",
+                principalTable: "Companies",
+                principalColumn: "Id");
+        }
+    }
+}
